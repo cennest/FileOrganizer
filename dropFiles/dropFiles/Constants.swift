cdc303c997dbc8c508d1d6a996976c7574e9e15c
kanchanWakchaure <kanchan.wakchaure@cennest.com>
2015-05-28 19:31:15 +0530

    Added constants

--- dropFiles/dropFiles/Constants.swift
+++ dropFiles/dropFiles/Constants.swift
@@ -27,6 +27,9 @@ struct User {
     }
 }
 
+struct CommonConstants {
+    static var kOk = "OK"
+}
 
 struct NSManagedObjectContextConstants {
     static var kUserEntity = "Users"
@@ -34,8 +37,12 @@ struct NSManagedObjectContextConstants {
 }
 
 struct UsersConstants {
+    static var kSuccessfulSignUpTitle = "Thank You!!!"
+    static var kFailureSignUpTitle = "User fail to register"
+    static var kSuccessfulSignInTitle = "Successfully Login"
+    static var kFailingSignInTitle = "Login Failure"
     static var kSuccessfulSignUpMessage = "Successfully Sign Up..\n\n\n Now, you can proceed for login!!!"
-    static var kErrorMessage = "Error"
+    static var kFailureSignUpMessge = "User registration fail"
     static var kSuccessfulSignInMessage = "You have been successfully logged in!!!"
     static var kInvalidUserName = "The user name is not valid"
     static var kInvalidPassword = "The password is not valid"
@@ -52,9 +59,23 @@ struct UsersConstants {
     static var kEnterEmailID = "Please, enter the email ID"
     static var kEnterFirstName = "Please, enter a first name"
     static var kEnterLastName = "Please, enter a last name"
-    static var kNameRegex = "^[a-z]{1,10}$"
-    static var kPasswordRegex = "[a-z0-9_-]{6,18}"
-    static var kUserNameRegex = "[a-z0-9_-]{3,16}"
+    static var kNameRegex = "^[a-zA-Z]{1,10}$"
+    static var kPasswordRegex = "[a-zA-Z0-9_-]{6,18}"
+    static var kUserNameRegex = "[a-zA-Z0-9_-]{3,16}"
     static var kEmailIDRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
     static var kPredicateFormat = "SELF MATCHES %@"
+}
+
+struct StorageServiceConstants {
+    static var kAzureAccountName = "fileorganizerdb"
+    static var kAzureAccountKey = "idzgO5aK5GUtZKIF7/6RA3KewWFULlwhA85dQeD7jNguRvwLIuZcd/6YiyWvUL7s4wdJNuD2qGqpqwWip2mSdA=="
+    static var kBlobContentType = "multipart/form-data; boundary=%@"
+    static var kBlobBoundry = "random string of your choosing"
+    static var kLogMessageForContainerFound = "%i containers were found…"
+    static var kLogMessageForBlobFoundMessage = "%i blobs were found in the images container…"
+    
+}
+
+struct FileOraganizerViewControllerConstants {
+    static var kStoryboardIdentifier = "fileViewController"
 }
